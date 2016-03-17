9fb773a6c30c628e819769ef7724685dae4fe594
Stewart Miles <smiles@google.com>
2016-03-16 17:29:37 -0700

    Integrate Latest @ 117395945
    Changes to analytics/testapp ...
    - Cleaned up Analytics sample.
    - Readme for the Firebase C++ Analytics test application.
    - Added gradle wrapper to Analytics sample to simplify the build process.
    - Removed debug println from Gradle build script.
    - Simplified Android Analytics sample project setup.
    
    Changes to auth/testapp ...
    - Add iOS version to testapp.
    
    Changes to invites/testapp ...
    - iOS version of the library and test app.
    
    CL: 117395945

---
+++ auth/testapp/src/ios/ios_main.mm
@@ -0,0 +1,79 @@
+// Copyright 2016 Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+#include <stdarg.h>
+
+#include "ios/ios_main.h"
+
+#include "main.h"
+
+#import <UIKit/UIKit.h>
+
+@interface FBIViewController : UIViewController
+@property(nonatomic) NSString* appName;
+@end
+
+extern "C" int common_main(int argc, const char* argv[]);
+
+@implementation FBIViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0),
+    ^{
+      const char* argv[] = {[self.appName UTF8String]};
+      common_main(1, argv);
+    });
+}
+
+@end
+
+static int exit_status = 0;
+
+// Log a message that can be viewed in "adb logcat".
+int LogMessage(const char* format, ...) {
+  va_list list;
+  int rc = 0;
+  va_start(list, format);
+  NSLogv(@(format), list);
+  va_end(list);
+  return rc;
+}
+
+int main(int argc, char* argv[]) {
+  @autoreleasepool {
+    UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+  return exit_status;
+}
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication*)application
+    didFinishLaunchingWithOptions:(NSDictionary*)launchOptions {
+  // Override point for customization after application launch.
+  // Sending invites requires a visible ViewController.
+  self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+  FBIViewController* viewController = [[FBIViewController alloc] init];
+  viewController.appName = @(FIREBASE_TESTAPP_NAME);
+  self.window.rootViewController = viewController;
+  [self.window makeKeyAndVisible];
+
+  return YES;
+}
+
+@end
