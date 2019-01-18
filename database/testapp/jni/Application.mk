30caa525b3749914442b21eb935f598d141eb18b
Anthony <amaurice@google.com>
2019-01-18 15:12:11 -0800

    Integrate Latest @ 228601768
    Changes to all ...
    - Changed gradle to use implementation instead of compile.
    - Update Firebase iOS to use 5.14.0.
    - Update Firebase Android dependencies, including removing
      firebase-common dependencies which is not needed.
    - Modify our testapps to use externalNativeBuild.ndkBuild
      rather than shellingout to call ndk-build.
    
    CL: 228601768

--- database/testapp/jni/Application.mk
+++ database/testapp/jni/Application.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-APP_PLATFORM:=android-14
+APP_PLATFORM:=android-16
 NDK_TOOLCHAIN_VERSION=clang
 APP_ABI:=armeabi-v7a arm64-v8a x86 x86_64
 APP_STL:=c++_static
