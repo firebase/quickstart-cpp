a6e95670d175f23ec243582d46f902e19c51cc63
Grant-Postma <grant.postma@gmail.com>
2020-05-28 16:31:25 -0400

    Class and resource files required for cocos2dx

---
+++ database/testapp/classes/AppDelegate.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "cocos2d.h"
+
+class AppDelegate : private cocos2d::Application {
+ public:
+  AppDelegate();
+  virtual ~AppDelegate();
+
+  virtual bool applicationDidFinishLaunching();
+  virtual void applicationDidEnterBackground();
+  virtual void applicationWillEnterForeground();
+};
---
+++ database/testapp/classes/TicTacToeScene.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "cocos2d.h"
+
+class TicTacToe : public cocos2d::Layer {
+ public:
+  static cocos2d::Scene* createScene();
+  virtual bool init();
+
+  CREATE_FUNC(TicTacToe);
+};
