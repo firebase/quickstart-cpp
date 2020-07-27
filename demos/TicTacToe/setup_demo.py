704175f01106080f84989c9c0d7507c902f7de9a
Grant-Postma <grant.postma@gmail.com>
2020-07-27 13:08:48 -0400

    Removing platform files. Modify file function.

--- demos/TicTacToe/setup_demo.py
+++ demos/TicTacToe/setup_demo.py
@@ -3,25 +3,19 @@
 
 # Copyright 2020 Google Inc. All rights reserved.
 #
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to
-# deal in the Software without restriction, including without limitation the
-# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-# sell copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
+# http://www.apache.org/licenses/LICENSE-2.0
 #
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-
+import fileinput
 import logging
 import os
 import subprocess
@@ -42,12 +36,16 @@ def logger_setup():
 
   return logger
 
-
 def log_run(dir, logger, cmd):
   # Logs the command.
   logger.info(cmd)
   # Runs the command.
   subprocess.call(cmd, cwd=dir, shell=True)
+
+def modify_proj_file(dir):
+  f = fileinput.FileInput(files = [os.path.join(dir,"main.cpp")], inplace = True)
+  for line in f:
+    print line.replace("AppDelegate.h", "app_delegate.h"),
 
 def main():
   """The main function."""
@@ -61,10 +59,13 @@ def main():
   game_name = "tic_tac_toe_demo"
   game_resources_dir = os.path.join(ROOT_DIRECTORY, "game_resources")
   game_files_dir = os.path.join(ROOT_DIRECTORY, game_name);
+  windows_proj_dir = os.path.join(game_files_dir,"proj.win32")
+  mac_proj_dir = os.path.join(game_files_dir,"proj.ios_mac","mac")
+  linux_proj_dir = os.path.join(game_files_dir,"proj.linux")
   build_dir = os.path.join(game_files_dir,"build")
   executable_dir = os.path.join(build_dir,"bin",game_name,"Debug")
 
- # Creating the cocos2d-x project. 
+  # Creating the cocos2d-x project. 
   log_run(ROOT_DIRECTORY, logger,"cocos new {0} -p com.DemoApp.{0} -l cpp -d .".format(game_name))
 
   # Removing the default cocos2d-x project files.
@@ -75,7 +76,10 @@ def main():
  
   # Copies the tic-tac-toe game files into the cocos2d-x project files.
   log_run(ROOT_DIRECTORY, logger, "cp {} {} -TRv".format(game_resources_dir,game_files_dir))
- 
+
+  # Changes the windows project main.cpp to include the new app_delegate header.
+  modify_proj_file(windows_proj_dir)
+
   # Changes directory into the build directory.
   log_run(build_dir, logger, 'cmake .. -G "Visual Studio 16 2019" -A Win32')
  
